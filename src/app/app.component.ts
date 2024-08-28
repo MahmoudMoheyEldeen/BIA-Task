af660a5bc7cf2e4af1f4a8e0dec706436bc71719
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-08-28 13:32:47 +0300

    installed and tried transloco

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import Swal from 'sweetalert2';
+import { TranslocoDirective, TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatIconModule, MatDividerModule],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    TranslocoDirective,
+    TranslocoPipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
