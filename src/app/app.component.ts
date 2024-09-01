2c4189ebeca0885387928763f8f4ab0e6f2f1825
MahmoudMoheyEldeen <mahmoudmohey1992@gmail.com>
2024-09-01 17:11:54 +0300

    changed direction in english

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import Swal from 'sweetalert2';
 import { TranslocoDirective, TranslocoPipe } from '@jsverse/transloco';
 import { SidebarComponent } from './shared-components/sidebar/sidebar.component';
 import { HeaderComponent } from './shared-components/header/header.component';
+import { LanguageServiceService } from './services/language-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -20,13 +22,23 @@ import { HeaderComponent } from './shared-components/header/header.component';
     TranslocoPipe,
     SidebarComponent,
     HeaderComponent,
+    CommonModule,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  currentLanguage: string = ''; /// to get observable  object
+
   title = 'BIA-Task';
   isCollapsedApp: boolean = false;
+
+  constructor(private languageService: LanguageServiceService) {
+    this.languageService.selectedLanguage$.subscribe((lang) => {
+      this.currentLanguage = lang;
+      console.log('from business component', this.currentLanguage);
+    }); /////to get observable  object
+  }
 
   ngOnInit(): void {
     this.toggleSidebar();
